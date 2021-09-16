46156ba8f99cef38f1fbbeb2317c5d8b05ed5325
Anton <antondanilin@gmail.com>
2021-09-16 15:54:37 +0300

    Это наш первый коммит

---
+++ lesson1/task1/index.js
@@ -0,0 +1,33 @@
+'use strict';
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// let arr = ['plum.png', 'orange.jpg','apple.bmp']
+
+
+// let answer = +prompt ("Are you 18?", "Yes")
+// // console.log(typeof(зу))
+// console.log(typeof(answer))
+// console.log(arr + 'hvost')
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+console.log("2" === 2);
+
+let isChecked = true;
+    isClose = true;
+
+console.log(isChecked || isClose)
+
