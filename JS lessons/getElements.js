7f6e37b9d48d8e2fcffb7a27205ca1bc48be42ff
antondanilin <90847675+antondanilin@users.noreply.github.com>
2021-10-22 18:29:36 +0300

    Законспектировал урок по получению элементов в коде js

--- JS lessons/getElements.js
+++ JS lessons/getElements.js
@@ -1,3 +1,13 @@
-let box = document.getElementById('box');
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneheart = document.querySelector('.heart')
+
 
 console.log(box);
+console.log(btn);
+console.log(btn[0]);
+console.log(circle[2]);
+console.log(heart);
+console.log(oneheart)
